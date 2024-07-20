690d56ed8680d8f677e947f0aa0eb2c4be0e8d39
vocabvictos <wuzhongheng@smail.nju.edu.cn>
2024-07-20 12:41:34 +0000

    Site updated: 2024-07-20 12:41:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/23/如何快速下载huggingface模型——全方法总结/"];function toRandomPost(){
+var posts=["2024/06/23/如何快速下载huggingface模型——全方法总结/","2024/07/20/GGUF格式大模型推理方法总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
